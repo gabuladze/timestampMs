dc2861fc2076f605253401463a864cb8eca93ec4
Giorgi Abuladze <g.abuladze@live.com>
2016-06-18 01:19:02 +0400

    Can now return proper timestamps and log errors

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 var express = require('express')
+var path = require('path');
+var moment = require('moment');
 var app = express()
 
+var port = process.env.PORT || 8000;
+
 app.get('/', function(req, res) {
-  res.send('Hello')
+  var file = path.join(__dirname, 'index.html')
+  res.sendFile(file, function(err) {
+    if (err) {
+      console.log(err)
+      res.sendStatus(err.status)
+    } else {
+      console.log("SENT " + file)
+    }
+  })
 })
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000!")
+app.get('/:dateString', function(req, res) {
+  var date;
+  if (/^\d+$/g.test(req.params.dateString)) {
+    var date = moment(req.params.dateString, 'X')
+  } else {
+    var date = moment(req.params.dateString, "MMMM DD, YYYY")
+  }
+
+  if(date.isValid()) {
+    res.json({
+      "unix": date.format("X"),
+      "natural": date.format("MMMM DD, YYYY")
+    })
+  } else {
+    res.json({
+      "unix": null,
+      "natural": null
+    })
+  }
+})
+
+app.listen(port, function() {
+  console.log("Server started on port " + port + "!" )
 })
